99307eb2286da3ea7309d442b881ddc7a9a9747a
Ekaterina <114247065+AndreevaEkaterina@users.noreply.github.com>
2023-03-25 01:33:46 +0800

    дополнительная задача

--- script.js
+++ script.js
@@ -18,7 +18,11 @@ if (day < 1 || day > 31) {
 } else if (day > 10 && day < 21) {
   alert("Число находится во второй декаде.");
 } else {
-    alert("Число находится в третьей декаде.");
+  alert("Число находится в третьей декаде.");
 }
 // решила 3
 const num3 = +prompt("Введите число: ");
+const units = num3 % 10;
+const ten = Math.trunc(num3 / 10) % 10;
+const hundreds = Math.trunc(num3 / 100) % 10;
+alert(`В числе ${num3} ${hundreds} сотен, ${ten} десятков, ${units} единиц.`)
